f5b9bda9214ebd2f558dde1f3c439e034de5affc
Kenny <kenny16372@gmail.com>
2025-12-02 07:54:09 +0100

    feat(day_2): Star 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,5 @@
+use std::iter::repeat;
+
 #[derive(Debug)]
 struct Range(std::ops::RangeInclusive<i64>);
 
@@ -23,13 +25,47 @@ fn is_valid(value: i64) -> bool {
     result
 }
 
+fn is_valid2(value: i64) -> bool {
+    let log10 = (value as f32).log10() as u32;
+    let num_digits = log10 + 1;
+    // println!("{value} {num_digits}");
+    let valid = !(1..=(num_digits / 2)).any(|repeat_length| {
+        let divisor = 10i64.pow(repeat_length);
+        let lower = value % divisor;
+        if lower * 10 < divisor {
+            return false;
+        }
+        let mut v = value;
+        while v != 0 {
+            let low = v % divisor;
+            // println!("{lower} {low} {v} {value}");
+            if low != lower {
+                return false;
+            }
+            v = v / divisor
+        }
+        // println!("{value} {divisor} {lower}");
+        true
+    });
+    // if !valid {
+    //     println!("{value}");
+    // }
+    valid
+}
+
 fn main() {
     let content = std::fs::read_to_string("data/input.txt").expect("should be able to read file");
     let ranges: Vec<Range> = content.trim().split(',').map(|s| s.into()).collect();
     println!("{:?}", ranges);
     let star_1: i64 = ranges
-        .into_iter()
-        .flat_map(|range| range.0.filter(|&v| !is_valid(v)))
+        .iter()
+        .flat_map(|range| range.0.clone().filter(|&v| !is_valid(v)))
         .sum();
     println!("{star_1}");
+    let star_2: i128 = ranges
+        .iter()
+        .flat_map(|range| range.0.clone().filter(|&v| !is_valid2(v)))
+        .map(|v| v as i128)
+        .sum();
+    println!("{star_2}");
 }
