c728e2b49194cc1704bad2fafb2db91415941771
Kenny <kenny16372@gmail.com>
2025-12-03 07:27:15 +0100

    feat(day_3): Star 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,75 +1,64 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    fmt::Display,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 #[derive(Debug)]
-struct Range(std::ops::RangeInclusive<i64>);
+struct BatteryBank {
+    batteries: Vec<i8>,
+}
 
-impl From<&str> for Range {
+impl From<&str> for BatteryBank {
     fn from(value: &str) -> Self {
-        let (s, e) = value.split_once('-').expect("should contain dash");
-        Self(s.parse().expect("start should be valid")..=e.parse().expect("end should be valid"))
+        Self {
+            batteries: value
+                .chars()
+                .map(|c| c.to_digit(10).expect("only numbers") as i8)
+                .collect(),
+        }
     }
 }
 
-fn is_valid(value: i64) -> bool {
-    let log10 = (value as f32).log10() as u32;
-    let num_digits = log10 + 1;
-    // println!("{value} {num_digits}");
-    if num_digits % 2 != 0 {
-        // println!("valid");
-        return true;
+impl BatteryBank {
+    fn find_first_digit(&self) -> (i8, usize) {
+        self.batteries
+            .iter()
+            .enumerate()
+            .rev()
+            .skip(1)
+            .map(|(idx, &val)| (val, idx))
+            .max_by_key(|&(val, _)| val)
+            .expect("shouldn't be empty")
     }
-    let upper = value / 10i64.pow(num_digits / 2);
-    let lower = value % 10i64.pow(num_digits / 2);
-    let result = upper != lower;
-    // println!("{upper} {lower} {result}");
-    result
-}
 
-fn is_valid2(value: i64) -> bool {
-    let log10 = (value as f32).log10() as u32;
-    let num_digits = log10 + 1;
-    // println!("{value} {num_digits}");
-    let valid = !(1..=(num_digits / 2)).any(|repeat_length| {
-        let divisor = 10i64.pow(repeat_length);
-        let lower = value % divisor;
-        if lower * 10 < divisor {
-            return false;
-        }
-        let mut v = value;
-        while v != 0 {
-            let low = v % divisor;
-            // println!("{lower} {low} {v} {value}");
-            if low != lower {
-                return false;
-            }
-            v = v / divisor
-        }
-        // println!("{value} {divisor} {lower}");
-        true
-    });
-    // if !valid {
-    //     println!("{value}");
-    // }
-    valid
+    fn find_second_digit(&self, start_idx: usize) -> i8 {
+        *self.batteries[(start_idx + 1)..]
+            .iter()
+            .max()
+            .expect("shouldn't be empty")
+    }
+
+    fn star_1(&self) -> i64 {
+        let (first, idx) = self.find_first_digit();
+        let second = self.find_second_digit(idx);
+        println!("{first} {second} {:?}", self);
+        first as i64 * 10 + second as i64
+    }
 }
 
 fn main() {
     let content = std::fs::read_to_string("data/input.txt").expect("should be able to read file");
-    let ranges: Vec<Range> = content.trim().split(',').map(|s| s.into()).collect();
-    println!("{:?}", ranges);
+    let banks: Vec<BatteryBank> = content
+        .trim()
+        .split_ascii_whitespace()
+        .map(|l| l.into())
+        .collect();
     let star_1_start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    let star_1: i64 = ranges
-        .iter()
-        .flat_map(|range| range.0.clone().filter(|&v| !is_valid(v)))
-        .sum();
+    let star_1: i64 = banks.iter().map(|b| b.star_1()).sum();
     let star_1_end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("{star_1} {:?}", star_1_end - star_1_start);
     let star_2_start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    let star_2: i128 = ranges
-        .iter()
-        .flat_map(|range| range.0.clone().filter(|&v| !is_valid2(v)))
-        .map(|v| v as i128)
-        .sum();
+    let star_2 = "";
     let star_2_end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("{star_2} {:?}", star_2_end - star_2_start);
 }
