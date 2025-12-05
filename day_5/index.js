e8f4f519aa56f200785f3d9f4c4b85872f2ea066
Kenny <kenny16372@gmail.com>
2025-12-05 20:04:07 +0100

    feat(day_5): Star 2

--- day_5/index.js
+++ day_5/index.js
@@ -4,6 +4,7 @@ import { readFileSync } from "node:fs";
 const db = new PGlite();
 
 const input = readFileSync("data/input.txt", "utf-8");
+// const input = readFileSync("data/input_small.txt", "utf-8");
 const [ranges, ids] = input.split("\n\n");
 
 await db.exec(
@@ -39,3 +40,17 @@ const star1 = await db.query(`
   WHERE EXISTS (SELECT 1/0 FROM range r WHERE r.range @> i.id);
 `);
 console.log(star1.rows[0].count);
+
+const star2 = await db.query(`
+  WITH multi AS (
+    SELECT range_agg(range) FROM (SELECT range FROM range)
+  ),
+  unnested AS (
+    SELECT unnest(range_agg) FROM multi
+  ),
+  size AS (
+    SELECT upper(unnest) - lower(unnest) AS size FROM unnested
+  )
+  SELECT sum(size) from size;
+`);
+console.log(star2.rows);
