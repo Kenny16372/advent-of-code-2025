cf3bdcddc5d1328465117666b7972202ae28c081
Kenny <kenny16372@gmail.com>
2025-12-05 19:44:10 +0100

    feat(day_5): Star 1

--- day_5/index.js
+++ day_5/index.js
@@ -1,19 +1,41 @@
 import { PGlite } from "@electric-sql/pglite";
+import { readFileSync } from "node:fs";
 
 const db = new PGlite();
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS todo (
-    id SERIAL PRIMARY KEY,
-    task TEXT,
-    done BOOLEAN DEFAULT false
+
+const input = readFileSync("data/input.txt", "utf-8");
+const [ranges, ids] = input.split("\n\n");
+
+await db.exec(
+  `
+  CREATE TABLE IF NOT EXISTS range (
+    range int8range
   );
-  INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
-  INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
-  INSERT INTO todo (task, done) VALUES ('Create a table', true);
-  INSERT INTO todo (task, done) VALUES ('Insert some data', true);
-  INSERT INTO todo (task) VALUES ('Update a task');
+` +
+    ranges
+      .split("\n")
+      .map((range) => {
+        const [low, high] = range.split("-");
+        return `INSERT INTO range VALUES ('[${low}, ${high}]'::int8range);`;
+      })
+      .join("\n")
+);
+
+await db.exec(
+  `
+  CREATE TABLE IF NOT EXISTS id (
+    id INT8
+  );
+` +
+    ids
+      .split("\n")
+      .filter(Boolean)
+      .map((id) => `INSERT INTO id VALUES (${id});`)
+      .join("\n")
+);
+
+const star1 = await db.query(`
+  SELECT COUNT(*) FROM id i
+  WHERE EXISTS (SELECT 1/0 FROM range r WHERE r.range @> i.id);
 `);
-const ret = await db.query(`
-  SELECT * from todo WHERE id = 1;
-`);
-console.log(ret.rows);
+console.log(star1.rows[0].count);
